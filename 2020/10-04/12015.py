775b4d92f06d8c2065efa37f2cb1d9b2fba667de
mi2ntae <kimmintae981019@gmail.com>
2021-07-09 21:45:49 +0900

    <백준> LIS

---
+++ 2020/10-04/12015.py
@@ -0,0 +1,33 @@
+# 가장 긴 증가하는 부분 수열 2  (LIS : Longest Increasing Subsequence)
+
+A = int(input())
+sub = list(map(int, input().split()))
+lis = []
+
+def lb(num):
+    beg = 0
+    end = len(lis)-1
+    if num < lis[0]:
+        lis[0] = num
+
+    while end-beg > 1:
+        mid = (end+beg)//2
+        if lis[mid] < num:
+            beg = mid
+        else:
+            end = mid
+
+    if lis[beg] < num:
+        lis[end] = num
+    
+
+for num in sub:
+    if len(lis) == 0:
+        lis.append(num)
+    else:
+        if lis[-1] < num:
+            lis.append(num)
+        else:
+            lb(num)
+
+print(len(lis))
