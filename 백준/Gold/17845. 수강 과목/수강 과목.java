00036a7f1fce8f1d6e5aa65a3d926a7f14f59206
mi2ntae <73164347+mi2ntae@users.noreply.github.com>
2023-03-08 15:17:54 +0900

    [Gold V] Title: 수강 과목, Time: 176 ms, Memory: 49484 KB -BaekjoonHub

---
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	private static int N, K;
+	private static int[][] scores;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		scores = new int[K][2];
+		for(int i = 0 ; i < K; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			for(int j = 0; j < 2; j++) scores[i][j] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[][] memo = new int[K+1][N+1];
+		for(int i = 1; i <= K; i++) {
+			for(int j = 1; j <= N; j++) {
+				if(memo[i-1][j] != 0 && memo[i][j] == 0) memo[i][j] = memo[i-1][j];
+				if(j < scores[i-1][1]) continue;
+				memo[i][j] = Math.max(memo[i-1][j], memo[i-1][j-scores[i-1][1]]+scores[i-1][0]);
+			}
+		}
+		
+		System.out.println(memo[K][N]);
+		br.close();
+		
+	}
+}
