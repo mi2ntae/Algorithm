3f7f00d42c432374101e84b57934413f6f17165e
mi2ntae <73164347+mi2ntae@users.noreply.github.com>
2023-03-28 11:31:25 +0900

    [Silver I] Title: RGB거리, Time: 88 ms, Memory: 12048 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,6 +1,6 @@
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-import java.io.BufferedReader;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -9,22 +9,23 @@ public class Main {
 		int[][] costs = new int[N][3];
 		for(int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			for(int j = 0; j < 3; j++) costs[i][j] = Integer.parseInt(st.nextToken());
+			costs[i][0] = Integer.parseInt(st.nextToken());
+			costs[i][1] = Integer.parseInt(st.nextToken());
+			costs[i][2] = Integer.parseInt(st.nextToken());
 		}
 		
 		int[][] memo = new int[N][3];
-		for(int i = 0; i < 3; i++) memo[0][i] = costs[0][i];
-		
+		memo[0][0] = costs[0][0];
+		memo[0][1] = costs[0][1];
+		memo[0][2] = costs[0][2];
 		for(int i = 1; i < N; i++) {
-			for(int j = 0; j <3; j++) {
-				if(j == 0) memo[i][j] = Math.min(memo[i-1][1], memo[i-1][2]);
-				else if(j == 1) memo[i][j] = Math.min(memo[i-1][0], memo[i-1][2]);
-				else memo[i][j] = Math.min(memo[i-1][0], memo[i-1][1]);
-				memo[i][j] += costs[i][j];
-			}
+			memo[i][0] = Integer.min(memo[i-1][1], memo[i-1][2]) + costs[i][0];
+			memo[i][1] = Integer.min(memo[i-1][0], memo[i-1][2]) + costs[i][1];
+			memo[i][2] = Integer.min(memo[i-1][0], memo[i-1][1]) + costs[i][2];
 		}
-		int ans = Integer.MAX_VALUE;
-		for(int i = 0; i < 3; i++) ans = memo[N-1][i] < ans ? memo[N-1][i] : ans;
+		int ans = memo[N-1][0];
+		ans = ans < memo[N-1][1] ? ans : memo[N-1][1];
+		ans = ans < memo[N-1][2] ? ans : memo[N-1][2];
 		System.out.println(ans);
 		br.close();
 	}
