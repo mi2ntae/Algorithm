8a1ea5c6a2535ef55a91aabb2ee7b29a70957241
mi2ntae <73164347+mi2ntae@users.noreply.github.com>
2024-09-09 17:24:42 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1192 ms, Memory: 336376 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -1,52 +1,49 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        int X = Integer.parseInt(st.nextToken());
-        ArrayList[] g = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) g[i] = new ArrayList<Integer>();
+        int X = Integer.parseInt(st.nextToken())-1;
 
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            g[A].add(B);
+        ArrayList[] g = new ArrayList[N];
+        for(int i = 0; i < N; i++) g[i] = new ArrayList<Integer>();
+        for(int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken())-1;
+            int b = Integer.parseInt(st.nextToken())-1;
+            g[a].add(b);
         }
 
-        Deque<int[]> q = new ArrayDeque<>();
-        boolean[] v = new boolean[N + 1];
-        int[] distance = new int[N + 1];
-
-        v[X] = true;
-        distance[X] = 0;
-        q.offer(new int[]{X, 0});
+        ArrayDeque<int[]> q = new ArrayDeque<>();
+        int[] dist = new int[N];
+        Arrays.fill(dist, 300000);
+        dist[X] = 0;
+        q.offer(new int[]{0, X});
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
-
-            for (Integer nxt : (ArrayList<Integer>) g[cur[0]]) {
-                if (v[nxt]) continue;
-                v[nxt] = true;
-                distance[nxt] = cur[1] + 1;
-                q.offer(new int[]{nxt, cur[1] + 1});
+            for(int nxt : (ArrayList<Integer>) g[cur[1]]) {
+                if(dist[nxt] > cur[0]+1) {
+                    dist[nxt] = cur[0]+1;
+                    q.offer(new int[]{cur[0]+1, nxt});
+                }
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            if (distance[i] == K) sb.append(i+"\n");
+        for(int i = 0; i < N; i++) {
+            if(dist[i] == K) sb.append((i+1)+"\n");
         }
-        if(sb.length() == 0) System.out.println(-1);
-        else System.out.println(sb);
+        System.out.println(sb.length() == 0 ? -1 : sb);
         br.close();
     }
-
 }
