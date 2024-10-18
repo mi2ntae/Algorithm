2a1a33c31ddaa1dcacfae25dcff2ba07d2e3d25c
mi2ntae <73164347+mi2ntae@users.noreply.github.com>
2024-10-19 04:06:03 +0900

    [Platinum V] Title: 찾기, Time: 380 ms, Memory: 75036 KB -BaekjoonHub

--- 백준/Platinum/1786. 찾기/찾기.java
+++ 백준/Platinum/1786. 찾기/찾기.java
@@ -1,43 +1,52 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.sql.SQLOutput;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String T = br.readLine();
-		char[] t = new char[T.length()];
-		for(int i = 0; i < T.length(); i++) t[i] = T.charAt(i);
-		String P = br.readLine();
-		char[] p = new char[P.length()];
-		for(int i = 0; i < P.length(); i++) p[i] = P.charAt(i);
-		
-		int[] fail = new int[p.length];
-		int j = 0;
-		for (int i = 1; i < p.length; i++) {
-			while (j > 0 && p[i] != p[j]) j = fail[j - 1];
-			
-			if (p[i] == p[j]) fail[i] = ++j;
-			else fail[i] = 0;
-		}
-		
-		int cnt = 0;
-		j = 0;
-		ArrayList<Integer> ans = new ArrayList<>();
-		for(int i = 0; i < t.length; i++) {
-			while (j > 0 && t[i] != p[j]) j = fail[j - 1];
-			if(t[i] == p[j]) {
-				if (j == p.length-1) {
-					cnt++;
-					ans.add(i-j+1);
-					j = fail[j];
-				} else ++j;
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		sb.append(cnt+"\n");
-		for(int a : ans) sb.append(a+" ");
-		System.out.println(sb);
-		br.close();
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String T = br.readLine();
+        String P = br.readLine();
+
+        int[] table = makeFailTable(P);
+        int idx = 0;
+        int cnt = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < T.length(); i++) {
+            while(idx > 0 && T.charAt(i) != P.charAt(idx)) {
+                idx = table[idx-1];
+            }
+
+            if(T.charAt(i) == P.charAt(idx)) {
+                if(idx == P.length()-1) {
+                    sb.append(i-idx+1+" ");
+                    idx = table[idx];
+                    cnt++;
+                } else {
+                    idx++;
+                }
+            }
+        }
+        sb.insert(0, cnt+"\n");
+        System.out.println(sb);
+    }
+
+    private static int[] makeFailTable(String target) {
+        int t = target.length();
+        int[] table = new int[t];
+        int idx = 0;
+
+        for(int i = 1; i < t; i++) {
+            while(idx > 0 && target.charAt(i) != target.charAt(idx)) {
+                idx = table[idx-1];
+            }
+
+            if(target.charAt(i) == target.charAt(idx)) {
+                idx++;
+                table[i] = idx;
+            }
+        }
+        return table;
+    }
 }
