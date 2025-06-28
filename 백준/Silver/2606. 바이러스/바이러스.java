b483b4a1e961828d3bd985d5a4b837c4441ef1de
mi2ntae <73164347+mi2ntae@users.noreply.github.com>
2025-06-28 21:45:02 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 11752 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,42 +1,39 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        ArrayList[] g = new ArrayList[N];
-        for(int i = 0; i < N; i++) g[i] = new ArrayList<Integer>();
 
-        for(int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int a = Integer.parseInt(st.nextToken())-1;
-            int b = Integer.parseInt(st.nextToken())-1;
-            g[a].add(b);
-            g[b].add(a);
-        }
-
-        int ans = 0;
-        Queue<Integer> q = new ArrayDeque<>();
-        boolean[] visited = new boolean[N];
-        visited[0] = true;
-        q.offer(0);
-        while(!q.isEmpty()) {
-            int cur = q.poll();
-            for(Integer nxt : (ArrayList<Integer>) g[cur]) {
-                if(visited[nxt]) continue;
-                ans++;
-                visited[nxt] = true;
-                q.offer(nxt);
-            }
-        }
-
-        System.out.println(ans);
-        br.close();
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int k = Integer.parseInt(br.readLine());
+    int p = Integer.parseInt(br.readLine());
+    ArrayList<Integer>[] g = new ArrayList[k];
+    for(int i = 0; i < k; i++) g[i] = new ArrayList<Integer>();
+    
+    for(int i = 0; i < p; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int a = Integer.parseInt(st.nextToken())-1;
+      int b = Integer.parseInt(st.nextToken())-1;
+      g[a].add(b);
+      g[b].add(a);
     }
+    boolean[] v = new boolean[k];
+    ArrayDeque<Integer> q = new ArrayDeque<>();
+
+    q.offer(0);
+    v[0] = true;
+
+    int ans = 0;
+    while(!q.isEmpty()) {
+      int cur = q.poll();
+      for(Integer nxt : (ArrayList<Integer>) g[cur]) {
+        if(v[nxt]) continue;
+        v[nxt] = true;
+        ans++;
+        q.offer(nxt);
+      }
+    }
+    System.out.println(ans);
+    br.close();
+  }
 }
